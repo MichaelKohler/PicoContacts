1d81625a6cedce4945752eda37fbbbf2238f9d68
Michael Kohler <mkohler@picobudget.com>
2014-06-18 00:23:27 +0200

    configure implemented

--- app/controllers/ConfigureController.js
+++ app/controllers/ConfigureController.js
@@ -1,3 +1,15 @@
-picoContactsApp.controller('ConfigureController', function() {
-    
+picoContactsApp.controller('ConfigureController', function($scope, $location) {
+    $scope.config = {};
+
+    $scope.saveConfiguration = function() {
+        localStorage.setItem('picoContactsServer', $scope.config.address);
+        localStorage.setItem('picoContactsUsername', $scope.config.username);
+        // TODO: save password??
+        localStorage.setItem('picoContactsMaster', $scope.config.master);
+        $scope.redirectToLocalContacts();
+    };
+
+    $scope.redirectToLocalContacts = function() {
+        $location.path('/localContacts');
+    };
 });

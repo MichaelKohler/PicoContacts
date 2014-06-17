d264be9d66481e83c1ae8fd8400a9eb472dfac48
Michael Kohler <mkohler@picobudget.com>
2014-06-18 00:36:08 +0200

    reset localStorage possibility for debuggin purposes

--- app/controllers/LocalContactsController.js
+++ app/controllers/LocalContactsController.js
@@ -1,3 +1,10 @@
-picoContactsApp.controller('LocalContactsController', function() {
-    
+picoContactsApp.controller('LocalContactsController', function($scope) {
+
+
+    // for debugging purposes only
+    $scope.resetStorage = function() {
+        localStorage.removeItem('picoContactsServer');
+        localStorage.removeItem('picoContactsUsername');
+        localStorage.removeItem('picoContactsMaster');
+    };
 });

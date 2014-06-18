df546c69e6d5a153b0e76466822bf7387088add2
Michael Kohler <mkohler@picobudget.com>
2014-06-19 01:55:31 +0200

    list local contacts

--- app/controllers/LocalContactsController.js
+++ app/controllers/LocalContactsController.js
@@ -1,2 +1,15 @@
 picoContactsApp.controller('LocalContactsController', function($scope) {
+    $scope.contacts = [];
+
+    var allContacts = navigator.mozContacts.getAll({sortBy: "familyName", sortOrder: "descending"});
+    allContacts.onsuccess = function(event) {
+        var cursor = event.target;
+        if (cursor.result) {
+            $scope.contacts.push(cursor.result);
+            cursor.continue();
+        }
+    }
+    allContacts.onerror = function() {
+        console.warn("Something went terribly wrong! :(");
+    }
 });
